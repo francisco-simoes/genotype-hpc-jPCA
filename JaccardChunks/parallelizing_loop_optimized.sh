d991d8a1fe4e0fbc4b2b8053663f299cb4d239a2
Francisco Simoes <francisconfqsimoes@gmail.com>
2020-07-21 18:44:01 +0200

    Added leftover vars part in .sh loop

--- JaccardChunks/parallelizing_loop_optimized.sh
+++ JaccardChunks/parallelizing_loop_optimized.sh
@@ -1,8 +1,18 @@
-chunknumber=2
+chunknumber=134 
 chunksize=10000
+leftover=$((1343816 - $chunknumber * $chunksize))
+echo 'chunknumber: $chunknumber'
+echo 'chunksize: $chunksize'
+echo 'leftover: $leftover'
+
 minvars=$(for x in $(eval echo {1..$chunknumber}); do echo $(($x * $chunksize + 1)); done)
+
 for minvar in $minvars
 do
 	echo $minvar
 	sbatch --job-name=jaccard_chunks /hpc/hers_en/fsimoes/execute.sh /hpc/hers_en/shared/wxs/miniconda3/bin/Rscript /hpc/hers_en/fsimoes/jPCA/JaccardChunks/jPCA_chunks_parallel_optimized.R $minvar $chunksize
 done
+
+#And now the leftover vars:
+minvar=$(($chunknumber * $chunksize + 1))
+sbatch --job-name=jaccard_chunks /hpc/hers_en/fsimoes/execute.sh /hpc/hers_en/shared/wxs/miniconda3/bin/Rscript /hpc/hers_en/fsimoes/jPCA/JaccardChunks/jPCA_chunks_parallel_optimized.R $minvar $leftover
