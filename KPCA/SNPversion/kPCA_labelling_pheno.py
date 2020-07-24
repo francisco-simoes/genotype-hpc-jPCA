788bbff3ed3df39309861da68a980b65d86e291d
Francisco Simoes <francisconfqsimoes@gmail.com>
2020-07-24 13:24:57 +0200

    Adapted pheno colorized plots to SNP version

--- KPCA/SNPversion/kPCA_labelling_pheno.py
+++ KPCA/SNPversion/kPCA_labelling_pheno.py
@@ -8,11 +8,11 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 # Load necessary settings
-N = jPCA_settings.N
+#N = jPCA_settings.N
 
 # Get labels' dataframe
 df = pd.read_csv('/hpc/hers_en/fsimoes/wxs/Relevant/mine_wxs_180919.postPCA.pheno', sep='\t')
-df = df.iloc[:N]
+#df = df.iloc[:N]
 #print(df)
 print(df.columns)
 phenos = df['pheno'].unique()
@@ -20,7 +20,7 @@ number_of_phenos = len(phenos)
 print('Phenos:', df['pheno'].unique())
 
 # Load the PC scores and create scores Dataframe.
-scores_matrix = np.load('/hpc/hers_en/fsimoes/logs/objects/kPCA_X_kpca_N={}.npy'.format(N))
+scores_matrix = np.load('/hpc/hers_en/fsimoes/logs/objects/SNPversion/kPCA_X_kpca.npy')
 #(One PC for each column).
 
 # Plot with colored classes
@@ -36,8 +36,8 @@ cb = plt.colorbar()
 loc = np.arange(0,max(labels),max(labels)/float(len(colors)))
 cb.set_ticks(loc)
 cb.set_ticklabels(phenos)
-plt.title('First two PCs for N={}'.format(N))
+plt.title('kpca: First two PCs for SNP burdens; pheno labels')
 plt.xlabel('PC1')
 plt.ylabel('PC2')
-plt.savefig(('/hpc/hers_en/fsimoes/logs/images/kPCA_pheno_colorized_N={}'.format(N)))
+plt.savefig('/hpc/hers_en/fsimoes/logs/images/kPCA_pheno_colorized_SNP.png')
 #plt.show()
